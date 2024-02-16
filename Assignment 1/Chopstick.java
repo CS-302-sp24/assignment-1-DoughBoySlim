e5924c0bd74741c5bbefe00909c6d07ac9b21887
DoughBoySlim <rx5857yk@go.minnstate.edu>
2024-02-16 16:47:39 -0600

    Editing to add variables only. See what happens

--- Assignment 1/Chopstick.java
+++ Assignment 1/Chopstick.java
@@ -1,5 +1,30 @@
 class Chopstick {
 	private int id;
+	private boolean held; // This is to tell if a chopstick is being held
 	public Chopstick(int id) { this.id = id; }
  	public int getId() { return id; }
+
+	// Writing a function that the philosopher can pick up a chopstick
+	public void Grab(int philosopherId) throws InterruptedException {
+
+		// If chopstick is take wait
+		while(held){
+			wait();
+		}
+
+		// If philosopher picks up chopstick, write to terminal to say thats what happened
+
+		held = true;
+		System.out.println("Philosopher" + philosopherId + "picked up chopstick" + id);
+	
+	}
+
+	// Function for the philosopher to let go of the chopstick when done eating
+
+	public void Drop(int philosopherId) {
+		// Letting Go of Chopstick
+		held = false;
+		System.out.println("Philosopher" + philosopherId + "dropped chopstick" + id);
+	}
+
 }
--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,9 +1,18 @@
 public class DiningPhilosophers {
 
     public static void main(String[] args) throws InterruptedException {
-      Philosopher[] philosophers = new Philosopher[5];
-      Chopstick[] chopsticks = new Chopstick[5];
+        // This is for setting the variables up for the main to pick up what happened in the function
+        int np = Integer.parseInt(args[0]); // # Philosopher
+        int nc = Integer.parseInt(args[1]); // # Cycles
+        int tt = Integer.parseInt(args[2]); // Thinking time
+        int et = Integer.parseInt(args[3]); // Eating Time
+        int rl = Integer.parseInt(args[4]); // Right or Left Handed
       
+
+        Philosopher[] philosophers = new Philosopher[np];
+        Chopstick[] chopsticks = new Chopstick[np];  
+
+
       for (int i = 0; i < 5; ++i)
         chopsticks[i] = new Chopstick(i);
       for (int i = 0; i < 5; ++i) {
