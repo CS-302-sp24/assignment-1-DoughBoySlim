ea3cc4fa00df848497fe0b9e9f389e4ab8442e78
DoughBoySlim <rx5857yk@go.minnstate.edu>
2024-02-16 17:18:05 -0600

    More changes to make the code work

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,25 +1,86 @@
 public class DiningPhilosophers {
+    public static void main(String[] args) {
+        // Parse command line arguments
+        if (args.length != 5) {
+            System.out.println("Usage: java DiningPhilosophers np nc tt et rl");
+            return;
+        }
 
-    public static void main(String[] args) throws InterruptedException {
-        // This is for setting the variables up for the main to pick up what happened in the function
-        int np = Integer.parseInt(args[0]); // # Philosopher
-        int nc = Integer.parseInt(args[1]); // # Cycles
-        int tt = Integer.parseInt(args[2]); // Thinking time
-        int et = Integer.parseInt(args[3]); // Eating Time
-        int rl = Integer.parseInt(args[4]); // Right or Left Handed
-      
+        int np = Integer.parseInt(args[0]); // Number of philosophers
+        int nc = Integer.parseInt(args[1]); // Number of cycles
+        int tt = Integer.parseInt(args[2]); // Maximum thinking time (milliseconds)
+        int et = Integer.parseInt(args[3]); // Maximum eating time (milliseconds)
+        int rl = Integer.parseInt(args[4]); // Right or left-handed
 
+        // Creating chopsticks
+        Chopstick[] chopsticks = new Chopstick[np];
+        for (int i = 0; i < np; i++) {
+            chopsticks[i] = new Chopstick(i);
+        }
+
+        // Creating philosophers
         Philosopher[] philosophers = new Philosopher[np];
-        Chopstick[] chopsticks = new Chopstick[np];  
+        for (int i = 0; i < np; i++) {
+            int leftChopstickIndex = i;
+            int rightChopstickIndex = (i + 1) % np;
+            if (rl == 1 && i % 2 != 0) { // Left-handed philosopher
+                leftChopstickIndex = (i + 1) % np;
+                rightChopstickIndex = i;
+            }
+            philosophers[i] = new Philosopher(i, chopsticks[leftChopstickIndex], chopsticks[rightChopstickIndex], nc, tt, et);
+        }
 
-
-      for (int i = 0; i < 5; ++i)
-        chopsticks[i] = new Chopstick(i);
-      for (int i = 0; i < 5; ++i) {
-        philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
-        philosophers[i].start();
-      }
-      for (int i = 0; i < 5; ++i)
-        philosophers[i].join();
+        // Start philosophers' threads
+        for (Philosopher philosopher : philosophers) {
+            philosopher.start();
+        }
     }
-  }
+}
+
+class Philosopher extends Thread {
+    private int id;
+    private Chopstick leftChopstick;
+    private Chopstick rightChopstick;
+    private int numCycles;
+    private int maxTt;
+    private int maxEt;
+
+    public Philosopher(int id, Chopstick leftChopstick, Chopstick rightChopstick, int numCycles, int maxTt, int maxEt) {
+        this.id = id;
+        this.leftChopstick = leftChopstick;
+        this.rightChopstick = rightChopstick;
+        this.numCycles = numCycles;
+        this.maxTt = maxTt;
+        this.maxEt = maxEt;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int cycle = 0; numCycles == 0 || cycle < numCycles; cycle++) {
+                // Thinking
+                int thinkingTime = (int) (Math.random() * maxTt);
+                System.out.println("Philosopher " + id + " thinks for " + thinkingTime + " units");
+                Thread.sleep(thinkingTime);
+
+                // Eating
+                System.out.println("Philosopher " + id + " wants right chopstick");
+                rightChopstick.Grab(id);
+                System.out.println("Philosopher " + id + " has right chopstick");
+                System.out.println("Philosopher " + id + " wants left chopstick");
+                leftChopstick.Grab(id);
+                System.out.println("Philosopher " + id + " has left chopstick");
+
+                int eatingTime = (int) (Math.random() * maxEt);
+                System.out.println("Philosopher " + id + " eats for " + eatingTime + " units");
+                Thread.sleep(eatingTime);
+
+                // Releasing chopsticks
+                leftChopstick.Drop(id);
+                rightChopstick.Drop(id);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -1,28 +1,35 @@
 import java.util.Random;
 
 class Philosopher extends Thread {
-  private Chopstick left, right;
-  private Random random;
-  private int thinkCount;
+    private Chopstick left, right;
+    private Random random;
+    private int thinkCount;
 
-  public Philosopher(Chopstick left, Chopstick right) {
-    this.left = left; this.right = right;
-    random = new Random();
-  }
+    public Philosopher(Chopstick left, Chopstick right) {
+        this.left = left;
+        this.right = right;
+        random = new Random();
+    }
 
-  public void run() {
-    try {
-      while(true) {
-        ++thinkCount;
-        if (thinkCount % 10 == 0)
-          System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
-          }
+    public void run() {
+        try {
+            while (true) {
+                ++thinkCount;
+                if (thinkCount % 10 == 0)
+                    System.out.println("Philosopher " + getId() + " has thought " + thinkCount + " times");
+                Thread.sleep(random.nextInt(1000));     // Think for a while
+                synchronized (left) {                    // Grab left chopstick
+                    System.out.println("Philosopher " + getId() + " wants left chopstick " + left.getId());
+                    synchronized (right) {                 // Grab right chopstick
+                        System.out.println("Philosopher " + getId() + " wants right chopstick " + right.getId());
+                        Thread.sleep(random.nextInt(1000)); // Eat for a while
+                    }
+                    System.out.println("Philosopher " + getId() + " released right chopstick " + right.getId());
+                }
+                System.out.println("Philosopher " + getId() + " released left chopstick " + left.getId());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-      }
-    } catch(InterruptedException e) {}
-  }
+    }
 }
